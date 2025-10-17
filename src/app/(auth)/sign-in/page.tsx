2f4b02663f7d9d5517e3076e3f60e934f45e73fc
AlbertGorgori <albert.gorgori@outlook.com>
2025-10-17 16:14:27 +0200

    feat: remove YourWorkouts component and related data, add new AuthForm component for sign-in and sign-up pages, implement Chat component for coach chat functionality

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import AuthForm from '@/components/AuthForm'
+import React from 'react'
+
+const page = () => {
+  return (
+    <section className="flex-center size-full max-sm:px-6">
+        <AuthForm />
+    </section>
+  )
+}
+
+export default page
